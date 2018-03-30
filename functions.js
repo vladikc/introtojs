6e891bad4eecc46ba9549e2704c7b581b60d4233
vladikc <vladikc91@gmail.com>
2018-03-30 19:15:36 -0400

    Invoice_app first commit

---
+++ functions.js
@@ -0,0 +1,24 @@
+
+var a = 3
+
+
+
+function  getUser(id,name,email){
+
+    var id = id
+    var name = name
+    var email = email 
+    console.log(`this is id id  ${id}...`) 
+
+
+}
+
+
+function sendBack(a){
+    return x *3
+}
+
+var b = sendBack(b)
+
+
+getUser("AB1234", "Jeremy", "janderson@techlaunch") 
---
+++ index.js
@@ -0,0 +1,28 @@
+var oper1 = 12 +12
+var oper2 = 25 -1
+var oper3 = 48/2
+var oper4 = 12*2
+
+console.log(oper2);
+
+var op1 =6 % 3
+var op2  = 10 % 2
+var op3 = 5 & 2
+
+
+var O1 = "Test Grades as fallows:"
+var O2 = " Student 1"
+var number =[0,10,20,30,40,50,60,70]
+
+
+console.log(number[2]+number[4])
+
+var teacher ={
+    firstName: "Jeremy Anderson",
+    age:30,
+    food: "steak",
+    drink: "Whiskey"
+
+}
+console.log(`Hi! Myname is ${teacher.firstName}, and I am
+ ${teacher["age"]} years old. I like to drink ${teacher.drink } with my ${teacher.food} .`)
